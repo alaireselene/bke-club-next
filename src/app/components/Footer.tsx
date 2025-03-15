7b2feafc2a6e25b52eaffee22fefd6ad95fb7e10
Truong-Son Nguyen <son.nt227148@sis.hust.edu.vn>
2025-03-15 22:26:05 +0700

    - Re-organize navbar - add Resource part

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -123,6 +123,7 @@ export function Footer() {
                   </a>
                 </li>
                 <li className="flex items-center">
+                  <Phone className="mr-3 h-5 w-5 text-slate-400" />
                   <a
                     href="tel:0983900429"
                     className="hover:text-cardinal-600 transition-colors"
